14c03b6f6c0d22ffd5a4fbd993a4e82979a41d5e
Matthew Rodgers <matthewdavidrodgers@gmail.com>
2021-01-08 20:24:06 -0500

    Add everything up through functions

--- lox/src/Lox.java
+++ lox/src/Lox.java
@@ -9,23 +9,26 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
-       if (args.length > 1) {
-           System.out.println("Usage: jlox [script]");
-           System.exit(64);
-       } else if (args.length == 1) {
-           runFile(args[0]);
-       } else {
-           runPrompt();
-       }
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
     }
 
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -44,10 +47,12 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        List<Stmt> statements = parser.parse();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        if (hadError) return;
+
+        interpreter.interpret(statements);
     }
 
     static void error(int line, String message) {
@@ -56,6 +61,7 @@ public class Lox {
 
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 
     static void error(Token token, String message) {
@@ -64,5 +70,10 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
--- tool/src/GenerateAst.java
+++ tool/src/GenerateAst.java
@@ -8,15 +8,30 @@ import java.util.List;
 public class GenerateAst {
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
-            System.err.println("Usage: generate_ast <output director>");
-            System.exit(64);
+              System.err.println("Usage: generate_ast <output director>");
+              System.exit(64);
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
-                "Binary   : Expr left, Token operator, Expr right",
-                "Grouping : Expr expression",
-                "Literal  : Object value",
-                "Unary    : Token operator, Expr right"
+              "Assign   : Token name, Expr value",
+              "Binary   : Expr left, Token operator, Expr right",
+              "Call     : Expr callee, Token paren, List<Expr> arguments",
+              "Grouping : Expr expression",
+              "Literal  : Object value",
+              "Logical  : Expr left, Token operator, Expr right",
+              "Unary    : Token operator, Expr right",
+              "Variable : Token name"
+        ));
+
+        defineAst(outputDir, "Stmt", Arrays.asList(
+              "Block      : List<Stmt> statements",
+              "Expression : Expr expression",
+              "Function   : Token name, List<Token> params, List<Stmt> body",
+              "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
+              "Print      : Expr expression",
+              "Var        : Token name, Expr initializer",
+              "Return     : Token keyword, Expr value",
+              "While      : Expr condition, Stmt body"
         ));
     }
 
