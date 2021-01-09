2d03da35d935bfae9ae143429e62fb349f63f9ed
Matthew Rodgers <matthewdavidrodgers@gmail.com>
2021-01-09 12:19:26 -0500

    Add variable resolution pass

--- lox/src/Environment.java
+++ lox/src/Environment.java
@@ -15,6 +15,15 @@ class Environment {
         this.enclosing = enclosing;
     }
 
+    private Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
+    }
+
     void define(String name, Object value) {
         values.put(name, value);
 
@@ -34,6 +43,10 @@ class Environment {
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
@@ -42,5 +55,9 @@ class Environment {
         if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
     }
 }
